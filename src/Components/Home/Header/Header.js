b3259738ee46f5ae394088490460e59b19679823
Abdur Rahman <a.rahman4124@gmail.com>
2021-04-15 00:01:43 +0600

    first commit

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderMain from "./HeaderMain/HeaderMain";
+import Menubar from "../../Common/Menubar/Menubar";
+
+const Header = () => {
+  return (
+    <div>
+      <Menubar />
+      <HeaderMain />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Home/Header/HeaderMain/HeaderMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderMain = () => {
+  return (
+    <div>
+      <h1>This is main header</h1>
+    </div>
+  );
+};
+
+export default HeaderMain;
